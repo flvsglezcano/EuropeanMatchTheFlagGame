e0cc14af5638aa648439481d374b62b1a96f2247
Anvesh Gangula <agangula@flvs.net>
2022-12-12 16:10:34 -0500

    session changes

--- src/components/GameRound.vue
+++ src/components/GameRound.vue
@@ -10,10 +10,18 @@ export default {
     name: 'GameRound',
     data(){
         return {
-           
+           totalScore:0
         }
     },
     methods:{ 
+        getSession(){
+
+        },
+        updateSession(round){
+            if(round.isCorrectAnswer){
+                this.totalScore=this.totalScore+20;
+            }
+        }
     }
 }
 </script>
--- src/components/StartGame.vue
+++ src/components/StartGame.vue
@@ -13,11 +13,7 @@ export default defineComponent({
     msg: String
   },
   data() {
-    return {
-      accessToken: "",
-      apiBaseUrl: "https://chat.openai.com/api",
-      backendApiBaseUrl: "https://chat.openai.com/backend-api",
-      userAgent:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    return { 
     }
   },
   methods: { 
