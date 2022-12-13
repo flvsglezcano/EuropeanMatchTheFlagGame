bfc01fa4c6e6ea071ea4753cf12ea331537f6b4b
Irina Popova <ipopova@flvs.net>
2022-12-13 09:15:46 -0500

    Uncomment button

--- src/components/StartGame.vue
+++ src/components/StartGame.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h1>Match the Flag to the European Country!</h1>
-    <!-- <button class="btn btn-primary" @click="$emit('beginGame')">
+   <button class="btn btn-primary" @click="$emit('beginGame')">
       Start Game
-    </button> -->
+    </button>
   </div>
 </template>
 
